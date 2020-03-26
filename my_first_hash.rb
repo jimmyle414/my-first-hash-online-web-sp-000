138bab52ccf98eede0613fb2aa5e8f921aa67d31
Jimmy Le <jimmyle414@gmail.com>
2020-03-26 00:31:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,8 +40,9 @@ def adding
                   "oil paintings" => 3
                  }
                  
-  shipping_manifest{"muskets" => 2, "gun powder" => 4}
-  return shipping_manifest[]
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  return shipping_manifest
   
   # add 2 muskets to the shipping_manifest hash below
 
